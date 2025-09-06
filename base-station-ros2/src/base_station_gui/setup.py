2d7f891a5ef8cc53ae17a0c4a182c23b7e7c4218
Braden Meyers <bjz-github@spwiz.com>
2025-09-06 10:24:15 -0600

    Changed location of launch file to be standard

--- base-station-ros2/src/base_station_gui/setup.py
+++ base-station-ros2/src/base_station_gui/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'base_station_gui'
 
@@ -16,6 +18,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'launch'),glob(os.path.join('launch','*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools', 'base_station_interfaces', 'cougars_interfaces'],
     zip_safe=True,
