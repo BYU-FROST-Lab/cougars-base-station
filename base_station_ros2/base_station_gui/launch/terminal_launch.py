2dae47596188d48097771a11da74072c7682123a
Ben Washburn <benrobertwashburn@gmail.com>
2025-09-25 10:21:05 -0600

    Got rid of teloep in launch file

--- base_station_ros2/base_station_gui/launch/terminal_launch.py
+++ base_station_ros2/base_station_gui/launch/terminal_launch.py
@@ -47,12 +47,6 @@ def generate_launch_description():
             name='base_station_wifi',
             parameters=[param_file]
         ),
-        launch_ros.actions.Node(
-            package='base_station_coms',
-            executable='teleop_couguv_key',
-            name='teleop_command',
-            parameters=[param_file]
-        ),
     ])
 
 if __name__ == '__main__':
